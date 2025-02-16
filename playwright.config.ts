8fa586f50bb728748687c5e87cae33002a8169e0
roman.ferber <roman.ferber@basis.com>
2025-02-16 15:44:53 -0500

    Test added

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,21 +22,37 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    headless: true,
   },
-
+  outputDir: 'test-results',
+  timeout:0,
+ 
+  
+  
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+        headless: false,
+        viewport: { width: 1720, height: 1080 },
+        ignoreHTTPSErrors: true,
+        acceptDownloads: true,
+        screenshot: `on`,
+        video: `on`,
+        trace: `on`,
+        launchOptions: {
+          slowMo: 0
+        }
+    },
     },
 
     // {
